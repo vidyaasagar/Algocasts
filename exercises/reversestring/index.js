f12710edba5230ee18ada9f54d53b5d5921c6faa
vidyasagar <vidyasagar484@gmail.com>
2018-05-29 13:19:04 +0530

    string reverse with test cases done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,10 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-    return str.split('').reverse().join('');
-}
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
 
+// function reverse(str) {
+//     let reverse = '';
+
+//     for(let character of str) {
+//         reverse = character + reverse;
+//     }
+// return reverse;
+// }
+
+function reverse(str) {
+    return str.split('').reduce((rev,char) => {
+        return char + rev;
+    },'');
+}
 module.exports = reverse;
 
 
